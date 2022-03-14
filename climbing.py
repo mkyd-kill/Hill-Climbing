d42d7dcfd36d7b07742b0d8b1cd47c854f7c4dfa
romeo mureithi <romeomureithi@gmail.com>
2022-03-14 18:24:38 +0300

    Added a modified simple hill-climbing & random restart

--- climbing.py
+++ climbing.py
@@ -1,78 +1,31 @@
-import numpy as np
+from numpy.random import randint
+from random import seed
 
+step_size = 0.1 # change it to 0.1
 
-def find_neighbours(state, landscape):
-    """ Finds the next neighbour's value state """
-    neighbours = []
-    dim = landscape.shape
+n_iter = 100
 
-    # left neighbour
-    if state[0] != 0:
-        neighbours.append((state[0] - 1, state[1]))
+def objective(x):
+    return 2 - (x**2)
 
-    # right neighbour
-    if state[0] != dim[0] - 1:
-        neighbours.append((state[0] + 1, state[1]))
+def hillclimbing(objective, n_iter, step_size):
+    solution = randint(-5, 6)
+    solution_eval = objective(solution)
+    score = solution_eval
 
-    # top neighbour
-    if state[1] != 0:
-        neighbours.append((state[0], state[1] - 1))
+    for i in range(n_iter):
+        candidate = solution + randint(-5, 6) * step_size
+        candidate_eval = objective(candidate)
 
-    # bottom neighbour
-    if state[1] != dim[1] - 1:
-        neighbours.append((state[0], state[1] + 1))
+        if candidate_eval >= solution_eval:
+            solution, solution_eval = candidate, candidate_eval
+            print('>%d f(%s) = %.5f' % (i, solution, solution_eval))
 
-    # top left
-    if state[0] != 0 and state[1] != 0:
-        neighbours.append((state[0] - 1, state[1] - 1))
+    return [solution, solution_eval]
 
-    # bottom left
-    if state[0] != 0 and state[1] != dim[1] - 1:
-        neighbours.append((state[0] - 1, state[1] + 1))
+seed(10)
 
-    # top right
-    if state[0] != dim[0] - 1 and state[1] != 0:
-        neighbours.append((state[0] + 1, state[1] - 1))
+best, best_score = hillclimbing(objective, n_iter, step_size)
 
-    # bottom right
-    if state[0] != dim[0] - 1 and state[1] != dim[1] - 1:
-        neighbours.append((state[0] + 1, state[1] + 1))
-
-    return neighbours
-
-
-# Current optimization objective: local/global maximum
-def hill_climb(curr_state, landscape):
-    neighbours = find_neighbours(curr_state, landscape)
-    bool
-    ascended = False
-    next_state = curr_state
-    for neighbour in neighbours: #Find the neighbour with the greatest value
-        if landscape[neighbour[0]][neighbour[1]] > landscape[next_state[0]][next_state[1]]:
-            next_state = neighbour
-            ascended = True
-
-    return ascended, next_state
-
-
-def __main__():
-    landscape = np.random.randint(1, high=50, size=(10, 10))
-    print(landscape)
-    start_state = (3, 6)  # matrix index coordinates
-    current_state = start_state
-    count = 1
-    ascending = True
-    while ascending:
-        print("\nStep #", count)
-        print("Current state coordinates: ", current_state)
-        print("Current state value: ", landscape[current_state[0]][current_state[1]])
-        count += 1
-        ascending, current_state = hill_climb(current_state, landscape)
-
-    print("\nStep #", count)
-    print("Optimization objective reached.")
-    print("Final state coordinates: ", current_state)
-    print("Final state value: ", landscape[current_state[0]][current_state[1]])
-
-
-__main__()
+print('\nOptimization Objective Reached!')
+print(f"Final State Coordinates {best} = {best_score}")
--- hill_climbing.py
+++ hill_climbing.py
@@ -1,8 +1,6 @@
 # hill climbing search of a one-dimensional objective function f(x) = 2 - x^2
 from numpy import asarray
-from numpy import arange
-from numpy.random import randn
-from numpy.random import rand
+from numpy.random import randint
 from numpy.random import seed
 
 """"
@@ -20,7 +18,7 @@ def objective(x):
 # hill climbing local search algorithm
 def hillclimbing(objective, bounds, n_iterations, step_size):
 	# generate an initial point
-	solution = bounds[:, 0] + rand(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
+	solution = bounds[:, 0] + randint(len(bounds)) * (bounds[:, 1] - bounds[:, 0])
 	# evaluate the initial point
 	solution_eval = objective(solution)
 	# run the hill climb
@@ -28,11 +26,11 @@ def hillclimbing(objective, bounds, n_iterations, step_size):
 	solutions.append(solution)
 	for i in range(n_iterations):
 		# take a step
-		candidate = solution + randn(len(bounds)) * step_size
+		candidate = solution + randint(len(bounds)) * step_size
 		# evaluate candidate point
 		candidte_eval = objective(candidate)
 		# check if we should keep the new point
-		if candidte_eval <= solution_eval:
+		if candidte_eval >= solution_eval:
 			# store the new point
 			solution, solution_eval = candidate, candidte_eval
 			# keep track of solutions
